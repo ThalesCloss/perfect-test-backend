8a6ce0e876c5c5bb1cc0f60e925b604e5448a09e
tcloss <thales.a.closs@gmail.com>
2021-07-23 11:28:26 -0400

    Feature: Roteamento para os controladores de Sale e Product

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@ Telas para ver o funcionamento sem dados
 Route::get('/', function () {
     return view('dashboard');
 });
-Route::get('/sales', function () {
-    return view('crud_sales');
-});
+Route::get('/sales', 'Sale@sale');
+Route::get('/sales/{id}', 'Sale@getSale');
 Route::get('/products', function () {
     return view('crud_products');
 });
@@ -22,3 +21,4 @@ Route::post('/products', 'Product@createProduct');
 Route::get('/products/{id}', 'Product@getProduct');
 Route::put('/products/{id}', 'Product@updateProduct');
 Route::post('/sales', 'Sale@createSale');
+Route::put('/sales/{id}', 'Sale@updateSale');
