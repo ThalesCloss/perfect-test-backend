cf3e4e3df06e29afda04d67e5f00db2a162b34c2
tcloss <thales.a.closs@gmail.com>
2021-07-22 22:08:42 -0400

    Feature: Suporte a edicao de produto

--- resources/views/crud_products.blade.php
+++ resources/views/crud_products.blade.php
@@ -4,24 +4,24 @@
 <h1>Adicionar / Editar Produto</h1>
 <div class='card'>
     <div class='card-body'>
-        <form method="POST">
+        <form method="POST" action="{{$id ?? ''}}">
+            @if (!empty($id))
+            @method('PUT')
+            @endif
             @csrf
-            @error('name')
-            <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
             <div class="form-group">
                 <label for="name">Nome do produto</label>
-                <input name="name" type="text" class="form-control " value="{{old('name')}}" id="name" />
+                <input name="name" type="text" class="form-control " value="{{$name ?? old('name')}}" id="name" />
             </div>
             <div class="form-group">
                 <label for="description">Descrição</label>
                 <textarea name="description" type="text" rows='5' class="form-control" id="description">
-                {{old('description')}}
+                {{$description ?? old('description')}}
                 </textarea>
             </div>
             <div class="form-group">
                 <label for="price">Preço</label>
-                <input name="price" type="text" class="form-control" id="price" placeholder="100,00 ou maior" value="{{old('price')}}" />
+                <input name="price" type="text" class="form-control" id="price" placeholder="100,00 ou maior" value="{{$price ?? old('price')}}" />
             </div>
 
             @if ($errors->any())
@@ -35,6 +35,7 @@
             @endif
 
             <button type="submit" class="btn btn-primary">Salvar</button>
+
         </form>
     </div>
 </div>
