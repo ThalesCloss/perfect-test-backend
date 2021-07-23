6911e7e4002aee2d095b5e8388658d34052fbf0d
tcloss <thales.a.closs@gmail.com>
2021-07-22 22:23:52 -0400

    Feature: Definicao do contrato para repositorios de Customer

---
+++ app/UseCases/Contracts/CustomerRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\UseCases\Contracts;
+
+use App\Entities\Customer;
+
+interface CustomerRepository
+{
+
+    public function save(Customer $customer): void;
+    public function getById(int $id): array;
+    public function getAll(): array;
+    public function delete(int $id): void;
+    public function getByEmailAndCpf($email, $cpf): void;
+}
