f8f16e450b1c477eeebc0781a545edfd582812c8
tcloss <thales.a.closs@gmail.com>
2021-07-23 11:11:08 -0400

    Feature: Definicao do repositorio para Sale

---
+++ app/UseCases/Contracts/SaleRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\UseCases\Contracts;
+
+use App\Entities\Sale;
+use DateTime;
+
+interface SaleRepository
+{
+
+    public function save(Sale $sale): void;
+    public function getById(int $id): array;
+    public function getAll(): array;
+    public function delete(int $id): void;
+    public function getSaleReport(): array;
+    public function getByCustomerPeriod(int $id, DateTime $initialDate, DateTime $endDate): array;
+}
