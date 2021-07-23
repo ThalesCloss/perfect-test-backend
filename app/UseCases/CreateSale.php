3e3226f7191e805a18c2be16ef124a8e32a8488d
tcloss <thales.a.closs@gmail.com>
2021-07-23 11:18:00 -0400

    Feature: Implmentacao do caso de uso de criacao de Sale

---
+++ app/UseCases/CreateSale.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\UseCases;
+
+use App\Entities\Customer;
+use App\Entities\Sale;
+use App\Repositories\EloquentRepositories\EloquentCustomerRepository;
+use App\Repositories\EloquentRepositories\EloquentProductRepository;
+use App\Repositories\EloquentRepositories\EloquentSaleRepository;
+use App\UseCases\Contracts\CustomerRepository;
+use App\UseCases\Contracts\ProductRepository;
+use App\UseCases\Contracts\SaleRepository;
+use App\ValueObjects\CPF;
+use App\ValueObjects\Email;
+
+class CreateSale
+{
+    private SaleRepository $saleRepository;
+    private CustomerRepository $customerRepository;
+    private ProductRepository $productRepository;
+    function __construct(
+        EloquentSaleRepository $saleRepository,
+        EloquentCustomerRepository $customerRepository,
+        EloquentProductRepository $productRepository
+    ) {
+        $this->saleRepository = $saleRepository;
+        $this->customerRepository = $customerRepository;
+        $this->productRepository = $productRepository;
+    }
+
+    function create(array $sale)
+    {
+        $email = Email::create($sale['email']);
+        $cpf = CPF::create($sale['cpf']);
+        $customer = $this->checkIfCustomerExists($email, $cpf);
+        if (empty($customer)) {
+            $customer = Customer::fromArray(['name' => $sale['name'], 'email' => $sale['email'], 'cpf' => $sale['cpf']]);
+            $this->customerRepository->save($customer);
+        }
+        $product = $this->productRepository->getById($sale['product_id']);
+        if (empty($product))
+            throw new \Error('O produto selecionado não está disponível para venda');
+
+        $saleEntity = Sale::fromArray([
+            'customer_id' => $customer->getId(),
+            'product_id' => $product['id'],
+            'unit_price' => $product['price'],
+            'amount' => $sale['amount'],
+            'discount' => $sale['discount'],
+            'sold_at' => $sale['sold_at'],
+            'status' => $sale['status']
+        ]);
+
+        $this->saleRepository->save($saleEntity);
+    }
+
+    function checkIfCustomerExists(Email $email, CPF $cpf)
+    {
+        $customer = $this->customerRepository->getByEmailAndCpf($email, $cpf);
+        if (!empty($customer))
+            return Customer::fromArray($customer, $customer['id']);
+        return null;
+    }
+}
